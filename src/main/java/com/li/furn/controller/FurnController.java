33186f8a93563544507c523195bd1f4d20911301
liyuelian <liyuelian.ke@outlook.com>
2023-03-10 20:17:02 +0800

    05-分页显示列表

--- src/main/java/com/li/furn/controller/FurnController.java
+++ src/main/java/com/li/furn/controller/FurnController.java
@@ -1,5 +1,8 @@
 package com.li.furn.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.li.furn.bean.Furn;
 import com.li.furn.bean.Msg;
 import com.li.furn.service.FurnService;
@@ -60,10 +63,34 @@ public class FurnController {
     }
 
     //根据id返回对应的furn对象
-    @RequestMapping("/find/{id}")
+    @GetMapping("/find/{id}")
     @ResponseBody
     public Msg findById(@PathVariable Integer id) {
         Furn furn = furnService.findById(id);
         return Msg.success().add("furn", furn);
+    }
+
+    /**
+     * 分页请求接口
+     *
+     * @param pageNum  要显示第几页
+     * @param pageSize 每一页显示的记录数
+     * @return
+     * @RequestParam 设置参数默认值
+     */
+    @ResponseBody
+    @RequestMapping("/furnsByPage")
+    public Msg listFurnsByPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                               @RequestParam(defaultValue = "5") Integer pageSize) {
+        //一、设置分页参数
+        //1.调用findAll完成查询，底层会进行物理分页，而不是逻辑分页
+        //2.也就是说会根据分页参数来计算limit ?,? 在发出sql语句时会带上limit
+        PageHelper.startPage(pageNum, pageSize);
+        List<Furn> furnList = furnService.findAll();
+        //二、将分页查询的结果封装到PageInfo对象中
+        //PageInfo对象包含了分页的各种信息，比如当前页数，每页的记录数，共有多少记录等等
+        PageInfo pageInfo = new PageInfo(furnList, pageSize);
+        //三、将PageInfo封装到Msg对象并返回
+        return Msg.success().add("pageInfo", pageInfo);
     }
 }
